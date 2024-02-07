d838b3fb3764188d1cf581350d5af7a1c500b52e
Jose Rodriguez <131327988+RodriguezJose92@users.noreply.github.com>
2024-02-07 09:27:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ async function serverData ({
 
   MudiExperience({
     tokenApi:'ATTkYedLSaMk3Jpe3stN',
-    skuNumber:'pruebadecoceramica',
+    skuNumber:document.querySelector('#refId').innerHTML.replace('REF ',''),
     idCompanyMudi:400,
     color:'#ae9c87',
     containerBtns:document.querySelector('#show'),
